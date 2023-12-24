5bb496c9c50762a562a3605427685c0a6108a884
ONEO <oneo.me@outlook.com>
2023-12-24 23:06:34 +0800

    Add package Avalonia.FluentUI.Icons

---
+++ packages/Avalonia.FluentUI.Icons/Avalonia.FluentUI.Icons.Sample/App.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.FluentUI.Icons.Sample;
+
+public class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            desktop.MainWindow = new MainWindow();
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ packages/Avalonia.FluentUI.Icons/Avalonia.FluentUI.Icons.Sample/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.FluentUI.Icons.Sample;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
