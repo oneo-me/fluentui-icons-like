ccf62753599fd405be975f5e0206c1c1f8f60326
ONEO <oneo.me@outlook.com>
2023-12-24 07:57:44 +0000

    Add builder

---
+++ build.ts
@@ -0,0 +1,6 @@
+import * as path from "path";
+
+const project = path.dirname(path.resolve(__dirname));
+const icons = path.join(project, "submodules/fluentui-system-icons/assets");
+
+console.log(icons);
